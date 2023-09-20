93ff5d354e73a49a4fe33db9507c868cb8f85014
Myakot <cstepan@yandex.ru>
2023-09-20 18:40:39 +0700

    brain-progression up-to-date, linter check done

--- brain_games/games/engine_progression.py
+++ brain_games/games/engine_progression.py
@@ -1,47 +1,24 @@
-from brain_games.engine import get_initial_randomized_number
-from brain_games.engine import welcome_get_name
-from brain_games.engine import getting_user_answer
-from brain_games.engine import show_correct_answer
-from brain_games.engine import return_win_end
-from random import choice
-from random import randint
+from random import randint, choice
 
 
-def question_answer_sequence(sequence):
-    answer = choice(sequence)
-    i = sequence.index(answer)
-    sequence_question = sequence[:i] + ['..'] + sequence[i + 1:]
-    sequence_question = ' '.join(str(x) for x in sequence_question)
-    return sequence_question, answer
+GAME_DESC = 'What number is missing in the progression?'
+
+
+def create_question_answer():
+    sequence = current_sequence()
+    correct_answer = choice(sequence)
+    i = sequence.index(correct_answer)
+    question = sequence[:i] + ['..'] + sequence[i + 1:]
+    question = ' '.join(str(x) for x in question)
+    return str(correct_answer), question
 
 
 def current_sequence():
     seq_length = randint(5, 10)
-    seq_step = get_initial_randomized_number()
-    sequence = [get_initial_randomized_number()]
+    seq_step = randint(1, 100)
+    sequence = [randint(1, 100)]
     x = 0
     while x <= seq_length:
         sequence.append(sequence[x] + seq_step)
         x += 1
     return sequence
-
-
-def main():
-    print('What number is missing in the progression?')
-    user_name = welcome_get_name()
-    counter = 0
-    while counter < 3:
-        question = question_answer_sequence(current_sequence())
-        (sequence_question, answer) = question
-        correct_answer = answer
-        print(f'Question: {sequence_question.strip()}')
-        user_answer = getting_user_answer()
-        if str(correct_answer) == user_answer:
-            print("Correct!\n")
-            counter += 1
-        else:
-            show_correct_answer(user_answer, correct_answer, user_name)
-            return
-
-    if counter == 3:
-        return_win_end(user_name)
