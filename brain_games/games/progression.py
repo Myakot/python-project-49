df2ef8283e78c2bb97b57b3d2edbecc04dccc286
Myakot <cstepan@yandex.ru>
2023-10-09 23:05:42 +0700

    can't even remember what has changed since last commit, but just in case

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,4 +1,4 @@
-from random import choice
+import random
 from brain_games.consts import GAME_GREET_PROGRESSION
 from brain_games.consts import PROGRESSION_SEQUENCE_LENGTH
 from brain_games.utils import get_num
@@ -14,28 +14,29 @@ def create_sequence():
         sequence.append(sequence_start)
     return sequence
 
+
 # По поводу прогрессии - она показалась мне несколько нагруженной, привожу в
 # пример несколько вариаций реализации для вдохновения:
-# def generate_progression(start_num, step, length):
-#     progression = []
-#     for i in range(start_num, length, step):
-#         progression.append(i)
-#     return progression
+def generate_progression(start_num, step, length):
+    progression = []
+    for i in range(start_num, length, step):
+        progression.append(i)
+    return progression
 # или же
 #
-#     first_num = get_random_number()
-#     diff = get_random_number()
-#     missed_num_ind = randint(0, PROGRESSION_LENGTH - 1)
-#
-#     progression = ' '.join([
-#         '..' if i == missed_num_ind else str(first_num + i * diff)
-#         for i in range(PROGRESSION_LENGTH)
-#     ])
-#     # вычисление пропущенного элемента оставлю за тобой)
-#     # но тут есть всё чтобы его найти
+    first_num = get_random_number()
+    diff = get_random_number()
+    missed_num_ind = randint(0, PROGRESSION_LENGTH - 1)
+    progression = ' '.join([
+        '..' if i == missed_num_ind else str(first_num + i * diff)
+        for i in range(PROGRESSION_LENGTH)
+    ])
+    # вычисление пропущенного элемента оставлю за тобой)
+    # но тут есть всё чтобы его найти
+
 def get_progression_and_missing_num():
     sequence = create_sequence()
-    correct_answer = choice(sequence)
+    correct_answer = random.choice(sequence)
     index_s = sequence.index(correct_answer)
     question = sequence[:index_s] + ['..'] + sequence[index_s + 1:]
     question = ' '.join(str(item) for item in question)
