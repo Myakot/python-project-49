913d20f799f26dedd655159de81f230e68642026
Myakot <cstepan@yandex.ru>
2023-10-17 14:38:31 +0700

    changed progression length constant to work with new code, added another function in progression.py for sequence generation

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,34 +5,27 @@ from brain_games.utils import get_num
 from brain_games.engine import run_game
 
 
+def generate_progression_items():
+    seq_start, seq_step = get_num(), get_num()
+    seq_length = PROGRESSION_SEQUENCE_LENGTH
+    return seq_start, seq_step, seq_length
+
+
+def generate_progression():
+    sequence = []
+    seq_start, seq_step, seq_length = generate_progression_items()
+    for i in range(seq_start, seq_length, seq_step):
+        sequence.append(i)
+    return sequence
+
+
 def get_progression_and_missing_num():
-    # sequence, seq_length = [], PROGRESSION_SEQUENCE_LENGTH
-    seq_step, seq_start = get_num(), get_num()
-    answ_position = get_num()
-    sequence = [i * seq_step for i in range (1, 11)]
-    correct_answer = str(sequence[answ_position])
-    sequence[answ_position] = '..'
-    question = ' '.join(map(str, sequence))
-    return question, correct_answer
-
-
-# def create_sequence():
-#     seq_length = PROGRESSION_SEQUENCE_LENGTH
-#     seq_step, sequence_start = get_num(), get_num()
-#     sequence = []
-#     for index in range(seq_length):
-#         sequence_start += seq_step
-#         sequence.append(sequence_start)
-#     return sequence
-
-
-# def get_progression_and_missing_num():
-#     sequence = create_sequence()
-#     correct_answer = random.choice(sequence)
-#     index_s = sequence.index(correct_answer)
-#     question = sequence[:index_s] + ['..'] + sequence[index_s + 1:]
-#     question = ' '.join(str(item) for item in question)
-#     return str(correct_answer), question
+    sequence = generate_progression()
+    correct_answer = random.choice(sequence)
+    index_s = sequence.index(correct_answer)
+    question = sequence[:index_s] + ['..'] + sequence[index_s + 1:]
+    question = ' '.join(str(item) for item in question)
+    return str(correct_answer), question
 
 
 def run_progression_game():
