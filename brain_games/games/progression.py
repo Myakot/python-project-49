c942dc032949dff02cecdd37d19bcd770e5ee83b
Myakot <cstepan@yandex.ru>
2023-10-23 20:43:47 +0700

    I'm sure this is the last commit, let's make it so

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -8,10 +8,6 @@ from brain_games.engine import run_game
 def generate_progression_items():
     seq_start, seq_step = get_random_num(), get_random_num()
     seq_length = PROGRESSION_SEQUENCE_LENGTH
-    return generate_progression(seq_start, seq_step, seq_length)
-
-
-def generate_progression(seq_start, seq_step, seq_length):
     return [seq_start + i * seq_step for i in range(seq_length)]
 
 
@@ -20,7 +16,8 @@ def get_progression_and_missing_num():
     index_s = random.randint(0, len(sequence) - 1)
     replaced_number = sequence[index_s]
     sequence[index_s] = '..'
-    return str(replaced_number), str(sequence)
+    sequence = ' '.join(map(str, sequence))
+    return str(replaced_number), sequence
 
 
 def run_progression_game():
